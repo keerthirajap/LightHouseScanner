1526b9297cc293691115d517fc617f8c47c071fc
KEERTHI RAJA <keerthiraja1988@gmail.com>
2024-07-29 01:53:10 +0530

    ui fixes

--- LightHouseScanner/wwwroot/service-worker.js
+++ LightHouseScanner/wwwroot/service-worker.js
@@ -1,19 +1,4 @@
-self.addEventListener('fetch', event => {
-    console.log('Fetch event for:', event.request.url);
-
-    event.respondWith(
-        fetch(event.request, { redirect: 'follow' })
-            .then(response => {
-                console.log('Fetch response status:', response.status, 'for URL:', event.request.url);
-                if (!response.ok) {
-                    console.error('Fetch error:', response.statusText);
-                    return new Response('Network error', { status: 500 });
-                }
-                return response;
-            })
-            .catch(error => {
-                console.error('Fetch error for URL:', event.request.url, 'Error:', error);
-                return new Response('Network error', { status: 500 });
-            })
-    );
-});
+// In development, always fetch from the network and do not enable offline support.
+// This is because caching would make development more difficult (changes would not
+// be reflected on the first load after each change).
+self.addEventListener('fetch', () => { });
