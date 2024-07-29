b7ae9206b341ff499e83d0286e2eb1730c850804
KEERTHI RAJA <keerthiraja1988@gmail.com>
2024-07-29 19:44:47 +0530

    UI Fixes

---
+++ LightHouseScanner/Models/SeoModel.cs
@@ -0,0 +1,37 @@
+﻿namespace LightHouseScanner.Models
+{
+    public class SeoModel
+    {
+        public string CanonicalURL { get; set; } = string.Empty;
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string SiteName { get; set; } = string.Empty;
+
+        public string OgImageURL { get; set; } = string.Empty;
+
+        public string OgImageURLAlt { get; set; } = string.Empty;
+
+        public string OgImageType { get; set; } = string.Empty;
+
+        public string OgImageHeight { get; set; } = string.Empty;
+
+        public string OgImageWidth { get; set; } = string.Empty;
+
+        public string OgType { get; set; } = string.Empty;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
+
+        public string TwitterSite { get; set; } = "@sitescan+";
+
+        public bool AllowRobots { get; set; } = true;
+
+        public List<string> Tags { get; set; } = new List<string>();
+
+        public List<string> SeeAlso { get; set; } = new List<string>();
+    }
+}
--- LightHouseScanner/Program.cs
+++ LightHouseScanner/Program.cs
@@ -5,7 +5,9 @@ namespace LightHouseScanner
     using Microsoft.AspNetCore.Components.Web;
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
     using Microsoft.Extensions.DependencyInjection;
+    using System.Net.Http.Json;
     using Toolbelt.Blazor.Extensions.DependencyInjection;
+    using static System.Net.WebRequestMethods;
 
     public class Program
     {
@@ -22,11 +24,21 @@ namespace LightHouseScanner
                 options.ContainerSelector = "#selector-of-container";
             });
 
+            var http = new HttpClient()
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            };
+
+            builder.Services.AddScoped(sp => http);
+
+            await ConfigureServices(builder, http);
+
             builder.Services.AddHttpClient("DefaultHttpClient", (sp, client) =>
             {
                 client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
                 client.EnableIntercept(sp);
             });
+
             builder.Services.AddHttpClient("ApiHttpClient", (sp, client) =>
             {
                 client.BaseAddress = new Uri("https://website-lighthouse-seo-scanner-online.niraiyaemailaccounts.workers.dev/");
@@ -39,6 +51,22 @@ namespace LightHouseScanner
             builder.UseLoadingBar();
             builder.Services.AddGoogleAnalytics("G-NWPMMWZY48");
             await builder.Build().RunAsync();
+        }
+
+        public static async Task ConfigureServices(WebAssemblyHostBuilder builder, HttpClient http)
+        {
+            string appSettingsFile = string.Empty;
+
+#if DEBUG
+            appSettingsFile = "appsettings.json";
+#else
+            appSettingsFile = $"appsettings.Production.json";
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
+#endif
+            using var response = await http.GetAsync(appSettingsFile);
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            builder.Configuration.AddJsonStream(stream);
         }
     }
 }
